528b3967f2c09d17bb9818bce5e62e674f1b6653
Eggmunkee <eggmunkee@gmail.com>
2023-12-13 17:19:13 -0600

    Adds media section w/ rumble players

--- dev/src/components/AMHighlights.vue
+++ dev/src/components/AMHighlights.vue
@@ -1,5 +1,35 @@
 <script setup>
+import { onMounted } from 'vue'
 import AMHighlight from './AMHighlight.vue'
+onMounted(() => {
+  console.log('AMHighlights.vue mounted')
+  !(function (r, u, m, b, l, e) {
+    ;(r._Rumble = b),
+      r[b] ||
+        (r[b] = function () {
+          ;(r[b]._ = r[b]._ || []).push(arguments)
+          if (r[b]._.length == 1) {
+            ;(l = u.createElement(m)),
+              (e = u.getElementsByTagName(m)[0]),
+              (l.async = 1),
+              (l.src =
+                'https://rumble.com/embedJS/ur4ql3' +
+                (arguments[1].video ? '.' + arguments[1].video : '') +
+                '/?url=' +
+                encodeURIComponent(location.href) +
+                '&args=' +
+                encodeURIComponent(JSON.stringify([].slice.apply(arguments)))),
+              e.parentNode.insertBefore(l, e)
+          }
+        })
+  })(window, document, 'script', 'Rumble')
+
+  setTimeout(() => {
+    // initialize rumble players
+    window.Rumble('play', { video: 'v3ygxcm', div: 'rumble_v3ygxcm' })
+    window.Rumble('play', { video: 'v3xxmbd', div: 'rumble_v3xxmbd' })
+  }, 200)
+})
 </script>
 
 <template>
@@ -95,9 +125,17 @@ import AMHighlight from './AMHighlight.vue'
       </p>
     </AMHighlight>
 
-    <hr />
-
-    <AMHighlight title="Under Development"> Thanks for visiting. (December 2023) </AMHighlight>
+    <AMHighlight title="Recent Media">
+      <p>"Biden Inc | Hunter | GOP Oversight | @Heritage FOIA dump"</p>
+      <div>
+        <div id="rumble_v3ygxcm"></div>
+      </div>
+      <br />
+      <p>"Rigged Elections, Lawsuits, and Support"</p>
+      <div>
+        <div id="rumble_v3xxmbd"></div>
+      </div>
+    </AMHighlight>
   </div>
 </template>
 
