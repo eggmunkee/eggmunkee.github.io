dcf5d62267d1d76051dde2b24a2b719fffcf7c4d
Eggmunkee <eggmunkee@gmail.com>
2026-07-26 23:32:10 -0500

    bg controls/gages

--- dev/src/views/MusicSR2026View.vue
+++ dev/src/views/MusicSR2026View.vue
@@ -111,15 +111,15 @@ import initialSongList from '../data/musicPlaylist_stochastic_recovery_20x6.json
                             &bullet;
                         </span>
                     </template>
-                    <span v-if="bgOn" class="bg-controls-guage-base-left">
+                    <span v-if="bgOn" class="bg-controls-guage-base-left" @click="incrementBgAnim(false, true)">
                         &nbsp;
                     </span>
-                    <span v-if="bgOn" class="white-dimmed bg-controls-guage-filled-left" style="" 
-                        :style="{'height': `${bgIndexProgress * 1.5}em` }">
+                    <span v-if="bgOn" class="white-dimmed bg-controls-guage-filled-left" @click="incrementBgAnim(false, true)" 
+                        :style="{'height': `${bgIndexProgress * 1.75}em` }">
                         &nbsp;
                     </span>
                     <span v-if="bgOn" class="bg-controls-guage-progress-left"
-                        :style="{'top': `${((2.1 + 0.75) - (bgAdvanceProgress * 0.65))}em`, 'height': `${bgAdvanceProgress * 1.3}em` }">
+                        :style="{'top': `${((2.2 + 0.75) - (bgAdvanceProgress * 0.7))}em`, 'height': `${bgAdvanceProgress * 1.4}em` }">
                         &nbsp;
                     </span>
                 </span>
@@ -169,15 +169,15 @@ import initialSongList from '../data/musicPlaylist_stochastic_recovery_20x6.json
                             &bullet;
                         </span>
                     </template>
-                    <span v-if="tintMode != TINT_OFF" style="" class="bg-controls-guage-base-right">
+                    <span v-if="tintMode != TINT_OFF" style="" class="bg-controls-guage-base-right" @click="updateColorTint">
                         &nbsp;
                     </span>
-                    <span v-if="tintMode != TINT_OFF" class="white-dimmed bg-controls-guage-filled-right" style="" 
-                        :style="{'height': `${tintIndexProgress * 1.5}em` }">
+                    <span v-if="tintMode != TINT_OFF" class="white-dimmed bg-controls-guage-filled-right" @click="updateColorTint" 
+                        :style="{'height': `${tintIndexProgress * 1.7}em` }">
                         &nbsp;
                     </span>
                     <span v-if="tintMode != TINT_OFF" class="bg-controls-guage-progress-right"
-                        :style="{'top': `${((2.1 + 0.75) - (tintAdvanceProgress * 0.65))}em`, 'height': `${tintAdvanceProgress * 1.3}em` }">
+                        :style="{'top': `${((2.2 + 0.75) - (tintAdvanceProgress * 0.7))}em`, 'height': `${tintAdvanceProgress * 1.4}em` }">
                         &nbsp;
                     </span>
                 </span>
@@ -906,15 +906,15 @@ export default {
         const bgCount = this.bgImages.length;
         if (bgCount > 0) {
             let idx = Math.min(this.currentBgIndex, bgCount - 1);
-            return 1.0 - (1.0 * idx / (bgCount - 1));
+            return 1.0 - ((idx) / (bgCount));
         }
         return 0.0;
     },
     tintIndexProgress() {
-        const tintCount = this.bgColors.length;
+        const tintCount = this.tintMode != TINT_NIGHT ? this.bgColors.length : this.bgColorsNight.length;
         if (tintCount > 0) {
             let idx = Math.min(this.bgColorIdx, tintCount - 1);
-            return 1.0 - (1.0 * idx / tintCount);
+            return 1.0 - ((idx) / (tintCount));
         }
         return 0.0;
     },
@@ -1393,7 +1393,7 @@ span.blue-dimmed-bg {
     width: 0;
     position: relative;
     font-size: 8pt;
-    text-shadow: 0 0.05em 0.5em rgba(255,255,255,70%);
+    text-shadow: 0 0.05em 0.5em rgba(63, 0, 0, 0.7);
 }
 .bg-controls-label.bg-controls-label-left {
     
@@ -1406,13 +1406,16 @@ span.blue-dimmed-bg {
     position: absolute; left: .5em; top: 0.15em;
 }
 .bg-ctrl-label-max {
-    color: #886000;
+    color: #cc9410;
+    font-weight: bolder;
 }
 .bg-ctrl-label-light {
+    font-style: italic;
     color: #557799;
 }
 .bg-ctrl-label-night {
     color: #cb1400;
+    text-shadow: 0 0.05em 0.5em rgba(255, 128, 0, 0.8);
 }
 
 .bg-controls-label-off {
@@ -1420,6 +1423,11 @@ span.blue-dimmed-bg {
     right: .35em;
     top: 0.15em;
     color:#181818;
+    font-style: italic;
+    text-shadow: 0 0.05em 0.5em rgba(255, 255, 255, 0.6);
+}
+.night-mode .bg-controls-label-off {
+    text-shadow: 0 0.05em 0.5em rgba(255, 128, 0, 0.8);
 }
 .bg-controls-label-off.label-right {
     left: .5em;
@@ -1450,50 +1458,56 @@ span.blue-dimmed-bg {
 }
 
 .bg-controls-guage-base-left {
-    position:absolute; top: 2em; left: .75em; padding: 0.15em 0; width: .5em; 
-    background: rgba(0,0,0,0.2); /* linear-gradient(to right, rgba(0,0,0,0.2), rgba(0,0,0,0.4), rgba(0,0,0,0.4), rgba(0,0,0,0.2));  */
+    position:absolute; top: 2.05em; left: .75em; padding: 0.1em 0; width: .65em; 
+    background: rgba(0,0,0,0.25);
     border-radius: .15em;
+}
+.night-mode .bg-controls-guage-base-left {
+    background: rgba(0,0,0,0.35);
 }
 
 .bg-controls-guage-progress-left {
-    position:absolute; left: 1.25em; width: .15em; 
-    border-right: 0.15em solid rgba(255,255,255,0.5);
+    position:absolute; top: 2.2em; left: 1.15em; width: .2em; 
+    border-right: 0.15em dashed rgba(255,255,255,0.35);
     transition: height 0.5s, top 0.5s;
 }
 .night-mode .bg-controls-guage-progress-left {
-    border-right: 0.15em solid rgba(252, 141, 90, 0.75);
+    border-right: 0.15em dashed rgba(252, 141, 90, 0.65);
 }
 .bg-controls-guage-progress-right {
-    position:absolute; top: 2.2em; right: 1.25em; width: .15em; 
-    border-right: 0.15em solid rgba(255,255,255,0.5);
+    position:absolute; top: 2.2em; right: 1.2em; width: .2em; 
+    border-right: 0.15em dashed rgba(255,255,255,0.35);
     transition: height 0.5s, top 0.5s;
 }
 .night-mode .bg-controls-guage-progress-right {
-    border-right: 0.15em solid rgba(252, 141, 90, 0.75);
+    border-right: 0.15em dashed rgba(252, 141, 90, 0.65);
 }
 
 .bg-controls-guage-filled-left {
-    position:absolute; left: .75em; top: 2.1em; width: .5em; 
-    background: linear-gradient(to bottom, rgba(219, 172, 178, 0.2), 50%, rgba(89, 194, 255, 0.4), 95%, rgba(89, 194, 255, 0.65)); 
+    position:absolute; left: .8em; top: 2.1em; width: .5em; 
+    background: linear-gradient(to bottom, rgba(219, 172, 178, 0.2), 30%, rgba(89, 194, 255, 0.4), 95%, rgba(89, 194, 255, 0.65)); 
     border-radius: .15em;
     transition: height 1s;
 }
 .night-mode .bg-controls-guage-filled-left {
-    background: linear-gradient(to bottom, rgba(63, 0, 8, 0.5), 50%, rgba(248, 182, 128, 0.5), 95%, rgba(248, 182, 128, 0.65));
+    background: linear-gradient(to bottom, rgba(63, 0, 8, 0.5), 30%, rgba(248, 182, 128, 0.5), 95%, rgba(248, 182, 128, 0.65));
 }
 .bg-controls-guage-base-right {
-    position:absolute; top: 2em; right: .75em; padding: 0.15em 0; width: .5em; 
+    position:absolute; top: 2.05em; right: .76em; padding: 0.1em 0; width: .65em; 
     background: rgba(0,0,0,0.2); /* linear-gradient(to right, rgba(0,0,0,0.2), rgba(0,0,0,0.4), rgba(0,0,0,0.4), rgba(0,0,0,0.2));  */
     border-radius: .15em;
 }
+.night-mode .bg-controls-guage-base-right {
+    background: rgba(0,0,0,0.35);
+}
 .bg-controls-guage-filled-right {
-    position:absolute; right: .75em; top: 2.1em; width: .5em; 
-    background: linear-gradient(to bottom, rgba(219, 172, 178, 0.2), 50%, rgba(89, 194, 255, 0.4), 95%, rgba(89, 194, 255, 0.65)); 
+    position:absolute; right: .8em; top: 2.1em; width: .5em; 
+    background: linear-gradient(to bottom, rgba(219, 172, 178, 0.2), 30%, rgba(89, 194, 255, 0.4), 95%, rgba(89, 194, 255, 0.65)); 
     border-radius: .15em;
     transition: height 1s;
 }
 .night-mode .bg-controls-label-right .bg-controls-guage-filled-right {
-    background: linear-gradient(to bottom, rgba(63, 0, 8, 0.5), 50%, rgba(248, 182, 128, 0.5), 95%, rgba(248, 182, 128, 0.65));
+    background: linear-gradient(to bottom, rgba(63, 0, 8, 0.5), 30%, rgba(248, 182, 128, 0.5), 95%, rgba(248, 182, 128, 0.65));
 }
 
 
